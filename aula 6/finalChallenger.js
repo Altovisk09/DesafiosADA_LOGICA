991ec7d7fcb305a8dca5a0d42cc50ff91cb5b2f0
Altovisk <ericaltao13@gmail.com>
2023-08-14 13:33:57 -0300

    base de dados pronta + funcoes

--- aula 6/finalChallenger.js
+++ aula 6/finalChallenger.js
@@ -3,7 +3,49 @@ const turmasBase = [
         id: 1,
         nome: "A",
         professor: "João Silva",
-        alunos: [1]
+        alunos: [1, 9, 17, 25, 33, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    },
+    {
+        id: 2,
+        nome: "B",
+        professor: "Mariana Oliveira",
+        alunos: [2, 10, 18, 26, 34]
+    },
+    {
+        id: 3,
+        nome: "C",
+        professor: "Pedro Santos",
+        alunos: [3, 11, 19, 27, 35]
+    },
+    {
+        id: 4,
+        nome: "D",
+        professor: "Isabela Almeida",
+        alunos: [4, 12, 20, 28, 36]
+    },
+    {
+        id: 5,
+        nome: "E",
+        professor: "Lucas Martins",
+        alunos: [5, 13, 21, 29, 37]
+    },
+    {
+        id: 6,
+        nome: "F",
+        professor: "Carolina Lima",
+        alunos: [6, 14, 22, 30, 38]
+    },
+    {
+        id: 7,
+        nome: "G",
+        professor: "Rafaela Rocha",
+        alunos: [7, 15, 23, 31, 39]
+    },
+    {
+        id: 8,
+        nome: "H",
+        professor: "Enzo Fernandes",
+        alunos: [8, 16, 24, 32, 40]
     }
 ];
 const alunosBase = [
@@ -17,10 +59,547 @@ const alunosBase = [
         nascimento: "15-03-2008",
         notas: [9.5, 8.7, 7.2],
         ativo: false
+    },
+    {
+        id: 2,
+        nome: "João",
+        sobrenome: "Silva",
+        cpf: "23456789011",
+        email: "joao.silva@example.com",
+        turma: 2,
+        nascimento: "05-07-2007",
+        notas: [8.0, 7.5, 6.9],
+        ativo: true
+    },
+    {
+        id: 3,
+        nome: "Mariana",
+        sobrenome: "Lima",
+        cpf: "34567890112",
+        email: "mariana.lima@example.com",
+        turma: 3,
+        nascimento: "20-01-2005",
+        notas: [6.7, 8.2, 9.0],
+        ativo: true
+    },
+    {
+        id: 4,
+        nome: "Pedro",
+        sobrenome: "Ferreira",
+        cpf: "45678901213",
+        email: "pedro.ferreira@example.com",
+        turma: 4,
+        nascimento: "10-11-2006",
+        notas: [7.8, 9.2, 8.5],
+        ativo: false
+    },
+    {
+        id: 5,
+        nome: "Julia",
+        sobrenome: "Santos",
+        cpf: "56789012314",
+        email: "julia.santos@example.com",
+        turma: 5,
+        nascimento: "25-09-2003",
+        notas: [9.0, 8.5, 7.7],
+        ativo: true
+    },
+    {
+        id: 6,
+        nome: "Lucas",
+        sobrenome: "Oliveira",
+        cpf: "67890123415",
+        email: "lucas.oliveira@example.com",
+        turma: 6,
+        nascimento: "08-02-2004",
+        notas: [7.5, 8.1, 6.4],
+        ativo: false
+    },
+    {
+        id: 7,
+        nome: "Isabela",
+        sobrenome: "Rocha",
+        cpf: "78901234516",
+        email: "isabela.rocha@example.com",
+        turma: 7,
+        nascimento: "30-12-2005",
+        notas: [6.8, 9.3, 8.7],
+        ativo: true
+    },
+    {
+        id: 8,
+        nome: "Enzo",
+        sobrenome: "Martins",
+        cpf: "89012345617",
+        email: "enzo.martins@example.com",
+        turma: 8,
+        nascimento: "15-06-2009",
+        notas: [9.2, 7.6, 8.4],
+        ativo: false
+    },
+    {
+        id: 9,
+        nome: "Sophia",
+        sobrenome: "Fernandes",
+        cpf: "90123456718",
+        email: "sophia.fernandes@example.com",
+        turma: 1,
+        nascimento: "18-04-2007",
+        notas: [8.7, 6.9, 7.8],
+        ativo: true
+    },
+    {
+        id: 10,
+        nome: "Matheus",
+        sobrenome: "Gomes",
+        cpf: "01234567819",
+        email: "matheus.gomes@example.com",
+        turma: 2,
+        nascimento: "03-09-2004",
+        notas: [7.2, 8.5, 6.1],
+        ativo: true
+    },
+    {
+        id: 11,
+        nome: "Rafaela",
+        sobrenome: "Sousa",
+        cpf: "23456789120",
+        email: "rafaela.sousa@example.com",
+        turma: 3,
+        nascimento: "21-05-2008",
+        notas: [8.9, 7.4, 9.3],
+        ativo: true
+    },
+    {
+        id: 12,
+        nome: "Pedro",
+        sobrenome: "Almeida",
+        cpf: "34567891221",
+        email: "pedro.almeida@example.com",
+        turma: 4,
+        nascimento: "14-07-2006",
+        notas: [6.5, 7.9, 8.1],
+        ativo: false
+    },
+    {
+        id: 13,
+        nome: "Camila",
+        sobrenome: "Machado",
+        cpf: "45678912322",
+        email: "camila.machado@example.com",
+        turma: 5,
+        nascimento: "02-11-2005",
+        notas: [9.4, 8.7, 7.0],
+        ativo: true
+    },
+    {
+        id: 14,
+        nome: "Vinicius",
+        sobrenome: "Farias",
+        cpf: "56789123423",
+        email: "vinicius.farias@example.com",
+        turma: 6,
+        nascimento: "19-01-2007",
+        notas: [7.3, 6.8, 8.6],
+        ativo: true
+    },
+    {
+        id: 15,
+        nome: "Laura",
+        sobrenome: "Ribeiro",
+        cpf: "67891234524",
+        email: "laura.ribeiro@example.com",
+        turma: 7,
+        nascimento: "05-04-2004",
+        notas: [8.3, 9.6, 7.5],
+        ativo: false
+    },
+    {
+        id: 16,
+        nome: "Diego",
+        sobrenome: "Costa",
+        cpf: "78912345625",
+        email: "diego.costa@example.com",
+        turma: 8,
+        nascimento: "30-08-2003",
+        notas: [6.2, 8.0, 9.1],
+        ativo: true
+    },
+    {
+        id: 17,
+        nome: "Beatriz",
+        sobrenome: "Santana",
+        cpf: "89123456726",
+        email: "beatriz.santana@example.com",
+        turma: 1,
+        nascimento: "17-10-2006",
+        notas: [7.1, 9.8, 6.7],
+        ativo: false
+    },
+    {
+        id: 18,
+        nome: "Lucas",
+        sobrenome: "Oliveira",
+        cpf: "90123456827",
+        email: "lucas.oliveira@example.com",
+        turma: 2,
+        nascimento: "12-12-2005",
+        notas: [8.5, 7.2, 9.0],
+        ativo: true
+    },
+    {
+        id: 19,
+        nome: "Marina",
+        sobrenome: "Carvalho",
+        cpf: "01234567928",
+        email: "marina.carvalho@example.com",
+        turma: 3,
+        nascimento: "09-03-2004",
+        notas: [9.7, 8.4, 6.9],
+        ativo: true
+    },
+    {
+        id: 20,
+        nome: "Gabriel",
+        sobrenome: "Silveira",
+        cpf: "12345679029",
+        email: "gabriel.silveira@example.com",
+        turma: 4,
+        nascimento: "25-05-2007",
+        notas: [6.8, 9.2, 7.6],
+        ativo: false
+    },
+    {
+        id: 21,
+        nome: "Larissa",
+        sobrenome: "Pereira",
+        cpf: "23456789130",
+        email: "larissa.pereira@example.com",
+        turma: 5,
+        nascimento: "07-09-2008",
+        notas: [8.6, 7.3, 9.0],
+        ativo: true
+    },
+    {
+        id: 22,
+        nome: "Lucas",
+        sobrenome: "Rocha",
+        cpf: "34567891231",
+        email: "lucas.rocha@example.com",
+        turma: 6,
+        nascimento: "20-11-2006",
+        notas: [7.9, 9.4, 8.1],
+        ativo: false
+    },
+    {
+        id: 23,
+        nome: "Amanda",
+        sobrenome: "Campos",
+        cpf: "45678912332",
+        email: "amanda.campos@example.com",
+        turma: 7,
+        nascimento: "03-02-2005",
+        notas: [6.8, 8.7, 9.3],
+        ativo: true
+    },
+    {
+        id: 24,
+        nome: "Matheus",
+        sobrenome: "Fernandes",
+        cpf: "56789123433",
+        email: "matheus.fernandes@example.com",
+        turma: 8,
+        nascimento: "18-04-2007",
+        notas: [9.1, 7.5, 8.4],
+        ativo: true
+    },
+    {
+        id: 25,
+        nome: "Juliana",
+        sobrenome: "Alves",
+        cpf: "67891234534",
+        email: "juliana.alves@example.com",
+        turma: 1,
+        nascimento: "30-06-2004",
+        notas: [7.2, 8.9, 6.7],
+        ativo: false
+    },
+    {
+        id: 26,
+        nome: "Gustavo",
+        sobrenome: "Lima",
+        cpf: "78912345635",
+        email: "gustavo.lima@example.com",
+        turma: 2,
+        nascimento: "15-08-2003",
+        notas: [8.3, 6.7, 9.5],
+        ativo: true
+    },
+    {
+        id: 27,
+        nome: "Isabella",
+        sobrenome: "Oliveira",
+        cpf: "89123456736",
+        email: "isabella.oliveira@example.com",
+        turma: 3,
+        nascimento: "28-10-2006",
+        notas: [6.5, 8.8, 7.9],
+        ativo: true
+    },
+    {
+        id: 28,
+        nome: "Rafael",
+        sobrenome: "Santos",
+        cpf: "90123456837",
+        email: "rafael.santos@example.com",
+        turma: 4,
+        nascimento: "12-12-2005",
+        notas: [9.0, 7.6, 8.2],
+        ativo: false
+    },
+    {
+        id: 29,
+        nome: "Carolina",
+        sobrenome: "Gonçalves",
+        cpf: "01234567938",
+        email: "carolina.goncalves@example.com",
+        turma: 5,
+        nascimento: "02-02-2004",
+        notas: [7.8, 8.9, 6.4],
+        ativo: true
+    },
+    {
+        id: 30,
+        nome: "Eduardo",
+        sobrenome: "Machado",
+        cpf: "12345679039",
+        email: "eduardo.machado@example.com",
+        turma: 6,
+        nascimento: "19-04-2007",
+        notas: [8.6, 9.3, 7.1],
+        ativo: true
+    },
+    {
+        id: 31,
+        nome: "Fernanda",
+        sobrenome: "Costa",
+        cpf: "23456789140",
+        email: "fernanda.costa@example.com",
+        turma: 7,
+        nascimento: "11-07-2008",
+        notas: [9.2, 7.5, 8.9],
+        ativo: true
+    },
+    {
+        id: 32,
+        nome: "Ricardo",
+        sobrenome: "Gomes",
+        cpf: "34567891241",
+        email: "ricardo.gomes@example.com",
+        turma: 8,
+        nascimento: "24-09-2006",
+        notas: [7.4, 8.1, 9.3],
+        ativo: false
+    },
+    {
+        id: 33,
+        nome: "Aline",
+        sobrenome: "Martins",
+        cpf: "45678912342",
+        email: "aline.martins@example.com",
+        turma: 1,
+        nascimento: "07-11-2005",
+        notas: [6.8, 8.7, 7.9],
+        ativo: true
+    },
+    {
+        id: 34,
+        nome: "Vitor",
+        sobrenome: "Ferreira",
+        cpf: "56789123443",
+        email: "vitor.ferreira@example.com",
+        turma: 2,
+        nascimento: "19-01-2007",
+        notas: [9.1, 7.6, 8.4],
+        ativo: true
+    },
+    {
+        id: 35,
+        nome: "Carla",
+        sobrenome: "Almeida",
+        cpf: "67891234544",
+        email: "carla.almeida@example.com",
+        turma: 3,
+        nascimento: "02-03-2004",
+        notas: [8.3, 6.9, 7.5],
+        ativo: false
+    },
+    {
+        id: 36,
+        nome: "Hugo",
+        sobrenome: "Sousa",
+        cpf: "78912345645",
+        email: "hugo.sousa@example.com",
+        turma: 4,
+        nascimento: "15-05-2003",
+        notas: [6.5, 8.2, 9.0],
+        ativo: true
+    },
+    {
+        id: 37,
+        nome: "Beatriz",
+        sobrenome: "Lima",
+        cpf: "89123456746",
+        email: "beatriz.lima@example.com",
+        turma: 5,
+        nascimento: "28-07-2006",
+        notas: [9.4, 7.8, 8.7],
+        ativo: true
+    },
+    {
+        id: 38,
+        nome: "Rafael",
+        sobrenome: "Ribeiro",
+        cpf: "90123456847",
+        email: "rafael.ribeiro@example.com",
+        turma: 6,
+        nascimento: "10-09-2005",
+        notas: [7.9, 8.6, 6.7],
+        ativo: false
+    },
+    {
+        id: 39,
+        nome: "Júlia",
+        sobrenome: "Campos",
+        cpf: "01234567948",
+        email: "julia.campos@example.com",
+        turma: 7,
+        nascimento: "23-11-2004",
+        notas: [8.5, 6.7, 9.2],
+        ativo: true
+    },
+    {
+        id: 40,
+        nome: "Gustavo",
+        sobrenome: "Oliveira",
+        cpf: "12345679049",
+        email: "gustavo.oliveira@example.com",
+        turma: 8,
+        nascimento: "06-01-2007",
+        notas: [7.3, 9.0, 8.4],
+        ativo: true
+    },
+    {
+        id: 41,
+        nome: "Ricardo",
+        sobrenome: "Sousa",
+        cpf: "34567890151",
+        email: "ricardo.sousa@example.com",
+        turma: 1,
+        nascimento: "05-07-2006",
+        notas: [8.0, 7.5, 6.9],
+        ativo: true
+    },
+    {
+        id: 42,
+        nome: "Carla",
+        sobrenome: "Campos",
+        cpf: "45678901252",
+        email: "carla.campos@example.com",
+        turma: 1,
+        nascimento: "20-01-2004",
+        notas: [6.7, 8.2, 9.0],
+        ativo: true
+    },
+    {
+        id: 43,
+        nome: "Vitor",
+        sobrenome: "Ferreira",
+        cpf: "56789012353",
+        email: "vitor.ferreira@example.com",
+        turma: 1,
+        nascimento: "10-11-2007",
+        notas: [7.8, 9.2, 8.5],
+        ativo: false
+    },
+    {
+        id: 44,
+        nome: "Bianca",
+        sobrenome: "Santos",
+        cpf: "67890123454",
+        email: "bianca.santos@example.com",
+        turma: 1,
+        nascimento: "25-09-2002",
+        notas: [9.0, 8.5, 7.7],
+        ativo: true
+    },
+    {
+        id: 45,
+        nome: "Rafael",
+        sobrenome: "Oliveira",
+        cpf: "78901234555",
+        email: "rafael.oliveira@example.com",
+        turma: 1,
+        nascimento: "08-02-2003",
+        notas: [7.5, 8.1, 6.4],
+        ativo: false
+    },
+    {
+        id: 46,
+        nome: "Isabela",
+        sobrenome: "Rocha",
+        cpf: "89012345656",
+        email: "isabela.rocha@example.com",
+        turma: 1,
+        nascimento: "30-12-2004",
+        notas: [6.8, 9.3, 8.7],
+        ativo: true
+    },
+    {
+        id: 47,
+        nome: "Enzo",
+        sobrenome: "Martins",
+        cpf: "90123456757",
+        email: "enzo.martins@example.com",
+        turma: 1,
+        nascimento: "15-06-2006",
+        notas: [9.2, 7.6, 8.4],
+        ativo: false
+    },
+    {
+        id: 48,
+        nome: "Sophia",
+        sobrenome: "Fernandes",
+        cpf: "01234567858",
+        email: "sophia.fernandes@example.com",
+        turma: 1,
+        nascimento: "18-04-2004",
+        notas: [8.7, 6.9, 7.8],
+        ativo: true
+    },
+    {
+        id: 49,
+        nome: "Matheus",
+        sobrenome: "Gomes",
+        cpf: "12345678959",
+        email: "matheus.gomes@example.com",
+        turma: 1,
+        nascimento: "03-09-2001",
+        notas: [7.2, 8.5, 6.1],
+        ativo: true
+    },
+    {
+        id: 50,
+        nome: "Rafaela",
+        sobrenome: "Sousa",
+        cpf: "23456789060",
+        email: "rafaela.sousa@example.com",
+        turma: 1,
+        nascimento: "21-05-2009",
+        notas: [8.9, 7.4, 9.3],
+        ativo: true
     }
 ];
-
-
 
 // Função para carregar turmas e alunos do localStorage
 function carregarDadosDoLocalStorage() {
@@ -254,11 +833,35 @@ const metodoAdm = {
     },
 
     ListaAlunosAtivos: function () {
-        
+        const alunosJSON = localStorage.getItem('alunos');
+        const alunos = JSON.parse(alunosJSON);
+        let alunosAtivos = alunos.filter(aluno => aluno.ativo);
+
+        if (alunosAtivos.length > 0) {
+            let output = `Lista de Alunos Ativos: ${alunosAtivos.length}\n\n`;
+            for (const aluno of alunosAtivos) {
+                output += `Nome: ${aluno.nome} ${aluno.sobrenome}, C Turma: ${aluno.turma}\n`;
+            }
+            alert(output);
+        } else {
+            alert("Não há alunos inativos.");
+        }
     },
 
     ListaAlunosInativos: function () {
-        // Lógica de retorno da lista de alunos inativos
+        const alunosJSON = localStorage.getItem('alunos');
+        const alunos = JSON.parse(alunosJSON);
+        let alunosInativos = alunos.filter(aluno => !aluno.ativo);
+
+        if (alunosInativos.length > 0) {
+            let output = `Lista de Alunos Inativos: ${alunosInativos.length}\n\n`;
+            for (const aluno of alunosInativos) {
+                output += `Nome: ${aluno.nome} ${aluno.sobrenome}, Matricula: ${aluno.id}, Turma: ${aluno.turma}\n`;
+            }
+            alert(output);
+        } else {
+            alert("Não há alunos inativos.");
+        }
     },
 
     AlunosMediaEsperada: function () {
@@ -336,11 +939,11 @@ function processo() {
                             validacoes.reset();
                             return;
                         case "8":
-                            metodoAdm.retornarListaAlunosAtivos();
+                            metodoAdm.ListaAlunosAtivos();
                             validacoes.reset();
                             return;
                         case "9":
-                            metodoAdm.retornarListaAlunosInativos();
+                            metodoAdm.ListaAlunosInativos();
                             validacoes.reset();
                             return;
                         case "10":
@@ -373,3 +976,4 @@ function processo() {
 }
 
 processo();
+
